384c4177cfd0d03fe8b82307618d6fd6c3e2b9df
AlbertChen <albirtle93@gmail.com>
2018-06-07 18:57:28 +0900

    get data
    get data fromeLendingClub web

---
+++ get and set data.R
@@ -0,0 +1,41 @@
+#setwd to github direction
+setwd("C:/Users/glab/Documents/GitHub/P2P_default_risk")
+#check dataset
+if (!file.exists("dataset")){
+        dir.create("dataset")
+}
+#set the download from web
+file2016Q1<-"https://resources.lendingclub.com/LoanStats_2016Q1.csv.zip"
+download.file(file2016Q1,destfile ="./dataset/2016Q1.csv",method = "curl" )
+
+file2016Q2<-"https://resources.lendingclub.com/LoanStats_2016Q2.csv.zip"
+download.file(file2016Q2,destfile ="./dataset/2016Q2.csv",method = "curl" )
+
+file2016Q3<-"https://resources.lendingclub.com/LoanStats_2016Q3.csv.zip"
+download.file(file2016Q3,destfile ="./dataset/2016Q3.csv",method = "curl" )
+
+file2016Q4<-"https://resources.lendingclub.com/LoanStats_2016Q4.csv.zip"
+download.file(file2016Q4,destfile ="./dataset/2016Q4.csv",method = "curl" )
+
+file2017Q1<-"https://resources.lendingclub.com/LoanStats_2017Q1.csv.zip"
+download.file(file2017Q1,destfile ="./dataset/2017Q1.csv",method = "curl" )
+
+file2017Q2<-"https://resources.lendingclub.com/LoanStats_2017Q2.csv.zip"
+download.file(file2017Q2,destfile ="./dataset/2017Q2.csv",method = "curl" )
+
+file2017Q3<-"https://resources.lendingclub.com/LoanStats_2017Q3.csv.zip"
+download.file(file2017Q3,destfile ="./dataset/2017Q3.csv",method = "curl" )
+
+file2017Q4<-"https://resources.lendingclub.com/LoanStats_2017Q4.csv.zip"
+download.file(file2017Q4,destfile ="./dataset/2017Q4.csv",method = "curl" )
+
+file2018Q1<-"https://resources.lendingclub.com/LoanStats_2018Q1.csv.zip"
+download.file(file2018Q1,destfile ="./dataset/2018Q1.csv",method = "curl" )
+#list all files in dataset
+list.files("./dataset")
+
+#check the date
+dateDownloaded<-data()
+
+#check data size
+object.size()
