7b11325fe6f96516db93e0d4892068cb063c33e9
Shin Fu Chen <albirtle93@gmail.com>
2018-09-14 15:00:24 +0800

    Update the comment

--- Feature_Selection.R
+++ Feature_Selection.R
@@ -1,10 +1,10 @@
 library(mRMRe)
 set.seed(342)
-# change to mRMR
+# change to mRMR.data
 f_data <- mRMR.data(data = data.frame(mRMRtest))
-#target_indices is loan_statule
+#target_indices is loan_statule, feature_count is the number we want to select
 results <- mRMR.classic("mRMRe.Filter", data = f_data, target_indices = 31,
                         feature_count = 10)
 solutions(results)
-#get the 
+#get the results 
 mrmatrix<-results@mi_matrix
