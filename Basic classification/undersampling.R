2cc579b9f6e825922fd30418a1f47096be5df840
AlbertChen <albirtle93@gmail.com>
2018-10-03 17:07:14 +0900

    fixed undersampling

--- Basic classification/undersampling.R
+++ Basic classification/undersampling.R
@@ -1,7 +1,7 @@
 #undersample
 
 library(unbalanced)
-input<-dataset2016[,-dataset2016$statue]# have to change to number
-Y<-dataset2016$statue
-data<-ubUnder(X=input,Y= Y,perc = 40,method="percPos")
-newData<-cbin(data$)
+input<-dataset2016[,-dataset2016$loan_status]# have to change to number
+Y<-dataset2016$loan_status
+data<-ubUnder(X=input,Y= Y,perc = 50,method="percPos")
+newData<-cbind(data$X,data$Y)
